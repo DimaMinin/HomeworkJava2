5fe7ad5eadb5bdf069ef6efef0adc6f192fbf660
Dima Minin <dminin0055@gmail.com>
2022-01-28 12:30:10 +0300

    Homework 7

---
+++ NetworkChatServer/src/com/Minin/server/ServerApp.java
@@ -0,0 +1,12 @@
+package com.Minin.server;
+
+import com.Minin.server.chat.MyServer;
+
+public class ServerApp {
+
+    private static final int DEFAULT_PORT = 8189;
+
+    public static void main(String[] args) {
+        new MyServer().start(DEFAULT_PORT);
+    }
+}
